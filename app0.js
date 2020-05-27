b7979e0ab34af583bce6493793d395eddb8368dd
deonoe <56742340+deonoe@users.noreply.github.com>
2020-05-28 01:56:45 +0200

    Security level 5

---
+++ app0.js
@@ -0,0 +1,98 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const md5 = require('md5');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+app.post('/register', function(req, res) {
+
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+
+      // Store hash in your password DB.
+
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+
+      newUser.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
+
+    });
+  });
+
+});
+
+app.post('/login', function(req, res) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  },
+  function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
+
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
