efc291585049c6222552356090b267fa0ed93d0b
deonoe <56742340+deonoe@users.noreply.github.com>
2020-05-27 17:12:45 +0200

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -26,8 +26,7 @@ const userSchema = new mongoose.Schema({
 
 // Add any other plugins or middleware here. For example, middleware for hashing passwords
 
-var secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
