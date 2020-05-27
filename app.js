a9c68af920a7ac49008dbeac8fc64bb793dd091c
deonoe <56742340+deonoe@users.noreply.github.com>
2020-05-27 22:45:51 +0200

    Security level 3

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 const app = express();
 
@@ -24,9 +25,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-// Add any other plugins or middleware here. For example, middleware for hashing passwords
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -47,7 +45,7 @@ app.post('/register', function(req, res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -62,7 +60,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if(err) {
@@ -76,11 +74,6 @@ app.post('/login', function(req, res) {
     }
   });
 });
-
-
-
-
-
 
 
 app.listen(3000, function() {
